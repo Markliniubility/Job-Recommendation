da41151ae6b336cf802876318b8f4b6224b6f18c
Markliniubility <limarksmail@gmail.com>
2020-11-02 00:50:45 -0500

    Initial commit

---
+++ src/main/java/com/laioffer/job/entity/Item.java
@@ -0,0 +1,63 @@
+package com.laioffer.job.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Item {
+    private String id;
+    private String title;
+    private String location;
+    private String companyLogo;
+    private String url;
+    private String description;
+    private Set<String> keywords;
+    private boolean favorite;
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("location")
+    public String getLocation() {
+        return location;
+    }
+
+    @JsonProperty("company_logo")
+    public String getCompanyLogo() {
+        return companyLogo;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
+    }
+}
